fd7105f7eeb17747f25393197af717d57a8d4fd2
Bryce Francom <97199545+OWBrony@users.noreply.github.com>
2022-04-23 16:28:36 -0600

    More updates.
    I added a readme file and provided the most up to date code file and executable.

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <string>
 using namespace std;
-
+// I am a comment.
+const float myNum = 3.14;
 int main() {
-    cout << "Hello World!";
+    string word1 = "";
+    string firstName = "John";
+    string lastName = "Doe";
+    string fullName = firstName+ " " + lastName;
+    int firstNumber = 1;
+    int x = 2, y = 3, z = 4;
+    /* This is one way to break a line \n is the most common way
+    to break the line.*/
+    cout << "Hello World! \n";
+    //This is another way to break the line.
+    cout << "this is a sample sentence!" << endl;
+    cout << "I am a new line!\n"; // comments can be here too
+    cout << "I am the first sentence with " << firstNumber << " variable.\n";
+    cout << x + y + z << "\n";
+    cout << "This is my constant: " << myNum << ".\n";
+    cout << "Type a word: ";
+    cin >> word1;
+    cout << "Your word is: " << word1;
+    cout << "\n" << fullName;
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# CPlusPlusHelloWorld
+# Overview
+
+I am attempting to learn the C++ language in an effort to be of more use to those
+who may need my services.
+
+This is merely a Hello World program along with a few other lines of practice code.
+
+As stated before, my purpose for making this was to learn the basics of the C++ language.
+
+[The demo video](https://youtu.be/7K3KFWsK1x0)
+
+# Development Environment
+
+This was developed using Visual Studio Code.
+
+This Hello World program was developed with the C++ language.
+
+# Useful Websites
+
+* [W3Schools](https://www.w3schools.com/CPP/default.asp)
